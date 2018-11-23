c069f43f38b9e7536b32e4cbc6da9cd053b119f6
nisol91 <nicolasolzi91@gmail.com>
2018-11-23 15:17:55 +0100

    2

--- es_3.js
+++ es_3.js
@@ -23,18 +23,29 @@ var studenti = [
 ];
 
 
-//credo un oggetto vuoto e poi sostituisco coi prompt cio che voglio che l utente inserisca e poi pusho l oggetto nell array degli studenti
+//creo un oggetto vuoto e poi sostituisco coi prompt cio che voglio che l utente inserisca e poi pusho l oggetto nell array degli studenti
 
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 1; i++) {
   var new_student = {
     'nome': '',
     'cognome': '',
     'età': 0,
   }
 
-  new_student.nome = prompt('Inserisci nome');
-  new_student.cognome = prompt('Inserisci cognome');
-  new_student.età = parseInt(prompt('Inserisci età'));
+  // new_student.nome = prompt('Inserisci nome');
+  // new_student.cognome = prompt('Inserisci cognome');
+  // new_student.età = parseInt(prompt('Inserisci età'));
+
+  // in alternativa faccio un for in su new_student
+
+  for (var key in new_student) {
+
+    if (key === 'età') {
+      new_student[key] = parseInt(prompt('Inserisci valore per: ' + key));
+    } else {
+      new_student[key] = prompt('Inserisci valore per: ' + key);
+    }
+  }
 
   studenti.push(new_student)
 }
@@ -42,3 +53,16 @@ for (var i = 0; i < 3; i++) {
 for (var i = 0; i < studenti.length; i++) {
   console.log(studenti[i].nome + ' ' + studenti[i].cognome + ' ' + studenti[i].età);
 }
+
+//trova eta media studenti
+
+var somma = 0;
+for (var i = 0; i < studenti.length; i++) {
+  somma += studenti[i].età;
+}
+
+console.log(somma);
+
+var media = somma/studenti.length;
+console.log(media);
+console.log(Math.floor(media));
